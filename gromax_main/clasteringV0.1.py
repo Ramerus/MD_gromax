e7f960c7f1d6a4b1cb7002ba213944e09b04e103
Ramerus <72870295+Ramerus@users.noreply.github.com>
2024-05-13 20:48:03 +0300

    Update clasteringV0.1.py

--- gromax_main/clasteringV0.1.py
+++ gromax_main/clasteringV0.1.py
@@ -9,15 +9,15 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import normalize
 
-exp_path = 'C:\\Users\\peter\\Desktop\\files\\my_soft\\drop_lib\\quartz_methane'
-#exp_path = 'C:\\Users\\peter\\Desktop\\files\\my_soft\\density_profile'
+exp_path = 'path to your experiment'
+
 dx = 0.12
 dir = '15'
 file_type = 'xtc'
 num_files = 2000
 fig_name = 'met_5'
 solid = 4.0 #hight of mineral(quartz) for type = 'angle'
-#solid = 0
+
 
 mass, mol_mas = gl.forcefield_loader()
 
@@ -35,25 +35,11 @@ except:
     print('No .xtc file! Check experimental directory!')
     exit()
 coords = np.dot(np.array(exp_data[exp_data.n_frames-1]._pos), 0.1)
-#exit()
-#aro_data = gl.aromatic_data(composition, coords, system_data)
 aro_data = gl.aro_data_old(coords, system_data)
-#scaler = StandardScaler()
-#x_scaled = scaler.fit_transform(aro_data)
-
-#x_normal = normalize(x_scaled)
-#x_normal = pd.DataFrame(x_normal)
-#pca = PCA(n_components=2)
-#x_principal = pca.fit_transform(x_normal)
 x_principal = pd.DataFrame(aro_data)
-#x_principal = x_normal
-#x_principal.columns = ['V1', 'V2']
-#print(x_principal.head())
-
 dbscan = DBSCAN(eps=0.4, min_samples=20).fit(x_principal)
 labels = dbscan.labels_
 aro_data['cluster'] = dbscan.labels_
-#print(aro_data.tail())
 
 
 
@@ -61,8 +47,7 @@ unique_labels = set(labels)
 colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
 core_samples_mask = np.zeros_like(labels, dtype=bool)
 core_samples_mask[dbscan.core_sample_indices_] = True
-#print(x_principal.loc[1, 'V1'])
-#exit()
+
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
 
@@ -83,7 +68,7 @@ for k, col in zip(unique_labels, colors):
             y.append(aro_data.loc[j, 'y'])
             z.append(aro_data.loc[j, 'z'])
 
-    #xy = x_principal[class_member_mask & core_samples_mask]
+    
     ax.scatter(
         x,
         y,
@@ -118,4 +103,3 @@ ax.set_ylabel('Y Label')
 ax.set_zlabel('Z Label')
 plt.title(f"Estimated number of clusters: {n_clusters_}")
 plt.show()
-#'''
